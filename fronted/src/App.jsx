a1bf9748303675d81e50f46a2f056fe50334710e
Rounok Paul <rounokpaul64@gmail.com>
2026-05-28 02:44:48 +0530

    Fix App css import

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './css/App.css'
+import "./Css/App.css";
 import MovieCard from './Components/MovieCard'
 import Home from './pages/Home'
 import Favorite from './pages/Favorite'
